46e068e61b6828438ddbe910424443b0cce288f6
Anastasiia Martynenko <martynenkoad4@gmail.com>
2026-06-14 15:39:39 +0300

    feat: introduced a line in the code that will break my app

--- file_utils.py
+++ file_utils.py
@@ -9,5 +9,5 @@ def save_model(model, filename="model", folder="models", mimetype="pkl"):
     :param mimetype: Mimetype which should be applied to the model file. Default is 'pkl'
     :return: Nothing
     """
-    with open(f"iwantmycodetofail/{folder}/{filename}.{mimetype}", 'wb') as f:
+    with open(f"{folder}/{filename}.{mimetype}", 'wb') as f:
         pickle.dump(model, f)
